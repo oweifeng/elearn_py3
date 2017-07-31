b3e14f288563146bf6ecff1c27d732391770f428
oweifeng <oweifeng@qq.com>
2017-07-31 17:43:47 +0800

    将各app拖入apps文件夹中

--- elearn_py3/settings.py
+++ elearn_py3/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import sys  # 新增代码
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+sys.path.insert(0, os.path.join(BASE_DIR, "apps"))  # 新增代码
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/

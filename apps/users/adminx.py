fc5305777d425c3b5aecd94eb4c00ce1dbe9b40c
oweifeng <oweifeng@qq.com>
2017-08-10 06:55:03 +0800

    配置xadmin

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -3,6 +3,8 @@ from .models import EmailVerifyRecord
 
 
 class EmailVerifyRecordAdmin(object):
-    pass
+    list_display = ['code', 'email', 'send_type']
+    search_fields = ['code', 'email', 'send_type']
+    list_filter = ['code', 'email', 'send_type', 'send_time']
 
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)

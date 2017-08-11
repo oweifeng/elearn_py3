12c0980951df0281730e9829ccbdf93e0108078b
oweifeng <oweifeng@qq.com>
2017-08-11 18:06:10 +0800

    管理后台的全局配置

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -1,9 +1,21 @@
 import xadmin
 from .models import EmailVerifyRecord, Banner
+from xadmin import views
+
+
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+
+
+class GlobalSettings(object):
+    site_title = "黄光部在线后台"
+    site_footer = "黄光部在线"
+    menu_style = "accordion"  # 将各app的model折叠起来
 
 
 class EmailVerifyRecordAdmin(object):
-    list_display = ['code', 'email', 'send_type']
+    list_display = ['code', 'email', 'send_type', 'send_time']
     search_fields = ['code', 'email', 'send_type']
     list_filter = ['code', 'email', 'send_type', 'send_time']
 
@@ -16,3 +28,5 @@ class BannerAdmin(object):
 
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
 xadmin.site.register(Banner, BannerAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)  # 注册basesetting
+xadmin.site.register(views.CommAdminView, GlobalSettings)  # 注册全局设定
